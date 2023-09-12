04a3d5aa2e9df4d13a86313d294bbcbbbd07b70f
Roman Grankov <grankov.r@promail366.com>
2023-09-12 23:28:25 +0300

    добавить функцию getData и теста для нее

---
+++ src/getData/getData.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+const mapArrToStrings = require('../mapArrToStrings/mapArrToStrings');
+
+const getData = async () => {
+    try {
+        const response = await axios.get(
+            'https://jsonplaceholder.typicode.com/users',
+        );
+        const usersIds = response.data.map((user) => user.id);
+        return mapArrToStrings(usersIds);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+module.exports = getData;
